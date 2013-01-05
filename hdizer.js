65fa139d00e3bb78e77f08b24a9ccc7f3cde433f
cbystrek <cbystrek@eden.rutgers.edu>
2013-01-05 16:39:07 -0500

    Version 0.0.0.8 Release
    -Extend functionality to https version of youtube
    -Now forces the playback quality to the maximum quality
    
    Complete redesign using youtube official js api.

--- hdizer.js
+++ hdizer.js
@@ -1,6 +1,17 @@
-var url = window.location.href;
-var hdizer = "&hd=1";
-if (url.indexOf("&hd=1") == -1)
-{
-        location.href= url + hdizer;
-}
+var onRdy = function onYouTubePlayerReady(playerId) {
+    ytplayer = document.getElementById("movie_player");
+    ytplayer.addEventListener("onStateChange", "onytplayerStateChange");
+};
+
+var onStateChange = function onytplayerStateChange(newState) {
+    if (newState == 1 && chromeExten != 1)
+    {
+        ytplayer = document.getElementById("movie_player");
+        ytplayer.setPlaybackQuality(ytplayer.getAvailableQualityLevels()[0]);
+        chromeExten = 1;
+    }
+ };
+
+var script = document.createElement("script");
+script.textContent = "var chromeExten = 0;\n" + onRdy.toString() +"\n" + onStateChange.toString();
+document.head.appendChild(script);
